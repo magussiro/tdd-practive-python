f000c9657c8dc45b34ab6c29d0bd2cc3b4bed441
Magus Liu <magus.siro@gmail.com>
2021-01-25 12:53:26 +0800

    init main

--- main.py
+++ main.py
@@ -5,13 +5,12 @@
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print("Hi, {0}".format(name))  # Press ⌘F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
+    test = WasRun("testRun")
+    print test.wasRun
+    test.testMethod()
+    print test.wasRun
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
