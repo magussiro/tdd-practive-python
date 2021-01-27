f0b6e8ea4e3f860ec63f888854a883ea371f3f00
Magus Liu <magus.siro@gmail.com>
2021-01-27 15:27:02 +0800

    set runConut=0 as default value, and increment it wher invoking testStarted

--- main.py
+++ main.py
@@ -55,7 +55,10 @@ class WasRun(TestCase):
 
 class TestResult:
     def __init__(self):
-        self.runCount = 1
+        self.runCount = 0
+
+    def testStarted(self):
+        self.runCount = self.runCount + 1
 
     def summary(self):
         return "%d run, 0 failed" % self.runCount
